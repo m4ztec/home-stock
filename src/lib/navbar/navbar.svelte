ce5516d981ba35a907ab20ced258f97c06214469
m4ztec <manueldias97@hotmail.com>
2024-12-04 20:05:07 +0000

    added navbar

---
+++ src/lib/navbar/navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar>
+    <NavBrand href="/">
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger  />
+    <NavUl >
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/docs/components/navbar">Navbar</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/login">Login</NavLi>
+    </NavUl>
+  </Navbar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import '../app.css';
+	import Navbar from '$lib/navbar/navbar.svelte';
+import '../app.css';
 	let { children } = $props();
 </script>
+
+<Navbar></Navbar>
 
 {@render children()}
