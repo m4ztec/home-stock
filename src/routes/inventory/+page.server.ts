baff537dc6630a01b3d0009c157614bdff6feb7e
m4ztec <manueldias97@hotmail.com>
2025-01-16 19:47:35 +0000

    finally created an inventory and display it

--- src/routes/inventory/+page.server.ts
+++ src/routes/inventory/+page.server.ts
@@ -3,29 +3,33 @@ import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
 import { eq } from "drizzle-orm";
 
-export const load: PageServerData = ()  => {
-	return {
-		post: {
-			title: `Title for hi goes here`,
-			content: `Content for  hi goes here`
-		},
-		inventories: {
-
+export const load = async ({locals})  => {
+	const sessionId = locals.session?.id!;
+	const [userId] = await db.select({userId: table.session.userId}).from(table.session).where(eq(table.session.id, sessionId)).limit(1);
+	const allInventories = await db.query.inventory_link.findMany({
+		columns: {userId: false, inventoryId: false},
+		where: eq(table.inventory_link.userId, userId.userId),
+		with: {
+			inventory: true
 		}
+	});
+	return {
+		inventories: allInventories
 	};
 };
 
 export const actions: Actions = {
-	cretae: async (event) => {
+	create: async (event) => {
 		const sessionId = event.locals.session?.id!;
-		const userId = db.select({userId: table.session.userId}).from(table.session).where(eq(table.session.id, sessionId));
+		const [userId] = await db.select({userId: table.session.userId}).from(table.session).where(eq(table.session.id, sessionId)).limit(1)
 		const formData = await event.request.formData();
 		const name = formData.get('name')?.toString();
 		const hi = {
 			name: name ?? '',
 		}
 
-	const inventoryId =	db.insert(table.inventoryTable).values(hi).returning({id: table.inventoryTable.id});
-	db.insert(table.inventory_link).values({})
+		const [inventoryId] = await db.insert(table.inventoryTable).values(hi).returning({id: table.inventoryTable.id});
+		console.log(userId.userId,inventoryId.id);
+		await db.insert(table.inventory_link).values({userId: userId.userId, inventoryId: inventoryId.id })
 	}
 };
--- src/routes/inventory/+page.svelte
+++ src/routes/inventory/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { PageServerData } from './$types';
+
+    import type { PageServerData } from './$types';
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox } from 'flowbite-svelte';
     
 	let { data }: { data: PageServerData } = $props();
@@ -12,34 +13,28 @@
 	</label>
 	<button>create inventory</button>
 </form>
-
-<div class="hi">{data.post}</div>
-
+   
 <Table hoverable={true}>
     <TableHead>
         <TableHeadCell class="!p-4">
         <Checkbox />
         </TableHeadCell>
-        <TableHeadCell>Product name</TableHeadCell>
-        <TableHeadCell>Color</TableHeadCell>
-        <TableHeadCell>Category</TableHeadCell>
-        <TableHeadCell>Price</TableHeadCell>
+        <TableHeadCell>inventory name</TableHeadCell>
         <TableHeadCell>
         <span class="sr-only">Edit</span>
         </TableHeadCell>
     </TableHead>
     <TableBody tableBodyClass="divide-y">
+        {#each data.inventories as inv}
         <TableBodyRow>
             <TableBodyCell class="!p-4">
                 <Checkbox />
             </TableBodyCell>
-            <TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
-            <TableBodyCell>Sliver</TableBodyCell>
-            <TableBodyCell>Laptop</TableBodyCell>
-            <TableBodyCell>$2999</TableBodyCell>
+            <TableBodyCell>{inv.inventory.name}</TableBodyCell>
             <TableBodyCell>
                 <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
             </TableBodyCell>
         </TableBodyRow>
+        {/each}
     </TableBody>
 </Table>
